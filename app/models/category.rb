3933f2b694fcad91fe18592f7443b8e2018f5abf
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2021-02-01 12:55:06 +0000

    feat: add many-to-many assoc between articles and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@
 
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
